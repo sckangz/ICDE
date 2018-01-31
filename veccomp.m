6a61e66add8fecd08dbe5871181b1a7cd16e7b4b
Zhao Kang 康昭 <zhao.kang@siu.edu>
2018-01-31 11:07:51 +0800

    Update veccomp.m

--- veccomp.m
+++ veccomp.m
@@ -1,5 +1,5 @@
 function [all,dis]=veccomp(ij,n,F,X);   
 for ji=1:n
-               all(ji)=norm(F(ij,:)-F(ji,:));
-               dis(ji)=norm(X(ij,:)-X(ji,:));
+               all(ji)=(norm(F(ij,:)-F(ji,:)))^2;
+               dis(ji)=(norm(X(ij,:)-X(ji,:)))^2;
               end
